b07e6bf83e31ef82189fd85dc4ce180bfe4b5908
Hatem <hattoum96@gmail.com>
2022-06-19 14:42:31 +0200

    initial commit

--- calls.py
+++ calls.py
@@ -40,7 +40,7 @@ def add_dialog(uuid: str, body: dict, auth_data: dict, cms: str):
     print(str(body))
     print(uuid_ent.text)
     print("-"*20)
-    if(uuid_ent.status_code == 500):
+    if(uuid_ent.status_code == 500 or uuid_ent.status_code == "500"):
         new_body = {str(key): str(value) for key, value in body.items()}
         new_uuid_ent = requests.post(call_url,json=new_body,headers=headers)
         print(str(new_body))

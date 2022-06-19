78d1486f8a723cfcbddb604708c14727d6c72bbf
Hatem <hattoum96@gmail.com>
2022-06-19 14:40:04 +0200

    initial commit

--- calls.py
+++ calls.py
@@ -38,8 +38,14 @@ def add_dialog(uuid: str, body: dict, auth_data: dict, cms: str):
     call_url = f"https://cms-v3.{cms}/api/v2/ext/dialog/dialogs-group-initial?agent_uuid={uuid}"
     uuid_ent = requests.post(call_url,json=body,headers=headers)
     print(str(body))
-    print("-"*20)
     print(uuid_ent.text)
+    print("-"*20)
+    if(uuid_ent.status_code == 500):
+        new_body = {str(key): str(value) for key, value in body.items()}
+        new_uuid_ent = requests.post(call_url,json=new_body,headers=headers)
+        print(str(new_body))
+        print(new_uuid_ent.text)
+        print("-"*20)
     return uuid_ent.status_code
 
 
